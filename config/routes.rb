989613faaaa29b8127128ddf79dc50bcc0712bad
Ryota <ryt.0081@gmail.com>
2016-10-08 20:15:16 +0900

    commentのroutingのせいせい

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  
+
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
 }
-  
-  resources :blogs, only: [:index, :new, :create, :edit, :update ,:destroy] do
+
+  resources :blogs do
+    resources :comments
+
     collection do
       post :confirm
     end
@@ -20,7 +22,7 @@ Rails.application.routes.draw do
   end
 
   root 'top#index'
-  
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
