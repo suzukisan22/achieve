18e60cd6c67266e35b25e596fa4c8d4f23dec8ff
Ryota <ryt.0081@gmail.com>
2016-10-10 17:03:54 +0900

    DIVE16 課題終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+    @followings = @user.followed_users
+    @followers = @user.followers
+  end
 end
