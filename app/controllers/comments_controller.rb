dc073c4c4695b53e3af6161b35b936528ddb8b15
Ryota <ryt.0081@gmail.com>
2016-10-16 17:38:10 +0900

    pusherのアラートを実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,9 @@ class CommentsController < ApplicationController
         format.json { render :show, status: :created, location: @comment }
         # JS形式でレスポンスを返します。
         format.js { render :index }
+        Pusher.trigger('test_channel', 'comment_created', {
+          message: 'あなたの作成したブログにコメントが付きました'
+        })
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
